c3d56c3520a283037880f13a7817dfb30c207f13
pauloovitorr <paulov.pv25@gmail.com>
2026-02-14 17:12:50 -0300

    implementação de login e autorização via token na API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\SeriesController;
 use App\Models\Episode;
 use App\Models\Series;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
@@ -12,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::controller(SeriesController::class)
-->group(function(){
+  ->middleware('auth:sanctum')
+  ->group(function () {
 
     Route::get('/series', 'index');
     Route::get('/series/{serie}', 'show');
@@ -20,20 +22,40 @@ Route::controller(SeriesController::class)
     Route::put('/series/{serie}', 'update');
     Route::delete('/series/{serie}', 'destroy');
 
-});
+  });
 
-Route::get('/series/{serie}/seasons', function(Series $serie){
-  return $serie->seasons;
-});
 
-Route::get('/series/{serie}/episodes', function(Series $serie){
-  return $serie->episodes;
-});
 
-Route::patch('/episodes/{episode}', function(Episode $episode, Request $request){
-  $episode->watched = $request->watched;
-  $episode->save();
+Route::middleware('auth:sanctum')
+  ->group(function () {
+    Route::get('/series/{serie}/seasons', function (Series $serie) {
+      return $serie->seasons;
+    });
 
-  return $episode;
+    Route::get('/series/{serie}/episodes', function (Series $serie) {
+      return $serie->episodes;
+    });
 
+    Route::patch('/episodes/{episode}', function (Episode $episode, Request $request) {
+      $episode->watched = $request->watched;
+      $episode->save();
+
+      return $episode;
+
+    });
+  });
+
+Route::post('/login', function (Request $request) {
+  $credentials = $request->only(['email', 'password']);
+
+  if (Auth::attempt($credentials) === false) {
+    return response()->json(['message' => 'Unauthorized'], 401);
+  }
+
+  $user = Auth::user();
+  $token = $user->createToken('token');
+
+  return response()->json([
+    'token' => $token->plainTextToken
+  ]);
 });
